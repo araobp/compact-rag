3b425e97e0b53defd7b6caa289f40425638df185
araobp <araobp@users.noreply.github.com>
2024-10-03 21:05:17 +0900

    Add test_vector_db.py

--- rag/embeddings.py
+++ rag/embeddings.py
@@ -1,12 +1,28 @@
 from openai import OpenAI
 
-EMBEDDINGS_MODEL = "text-embedding-3-small"
+EMBEDDING_MODEL = "text-embedding-3-small"
+
+_DIMENSIONS = {
+        "text-embedding-3-small": 1536,
+        "text-embedding-3-large": 3072
+        }
+
+DIMENSION = _DIMENSIONS[EMBEDDING_MODEL]
 
 client = OpenAI()
 
-def get_embedding(text, model=EMBEDDINGS_MODEL):
+def get_embedding(text, model=EMBEDDING_MODEL):
    return client.embeddings.create(input = [text], model=model).data[0].embedding
+
+def ui():
+
+    while True:
+        print("Text: ", end="")
+        text = input()
+        print("")
+        vector = get_embedding(text)
+        print(f"Embeddings: \n{vector}", end="\n\n")
 
 
 if __name__ == '__main__':
-    print(get_embedding('hello world'))
+    ui()
---
+++ rag/vector_db.py
@@ -0,0 +1,81 @@
+import struct
+from typing import List
+
+LIB_PATH = "../lib_raspi"
+
+#---- Reference: sqlite_vec __init__.py 
+
+from os import path
+import sqlite3
+
+__version__ = "0.1.3"
+__version_info__ = tuple(__version__.split("."))
+
+def loadable_path():
+    """ Returns the full path to the sqlite-vec loadable SQLite extension bundled with this package """
+
+    #loadable_path = path.join(path.dirname(__file__), "vec0")
+    loadable_path = path.join(LIB_PATH, "vec0") 
+    return path.normpath(loadable_path)
+
+def load(conn: sqlite3.Connection)  -> None:
+  """ Load the sqlite-vec SQLite extension into the given database connection. """
+
+  conn.load_extension(loadable_path())
+
+#---- The code below is my original
+
+def serialize_f32(vector: List[float]) -> bytes:
+    """serializes a list of floats into a compact "raw bytes" format"""
+    return struct.pack("%sf" % len(vector), *vector)
+
+
+class VectorDB:
+
+    def __init__(self, db_path, collection, dimensions):
+        self.db_path = db_path
+        self.db = sqlite3.connect(db_path)
+        self.db.enable_load_extension(True)
+        #sqlite_vec.load(self.db)
+        load(self.db)
+        self.db.enable_load_extension(False)
+        self.db.execute(
+            f"CREATE VIRTUAL TABLE IF NOT EXISTS {collection} USING vec0(embedding float[{dimensions}])"
+        )
+        self.collection = collection
+
+    def delete_all(self):
+        with self.db:
+            self.db.execute(f"DELETE FROM {self.collection}")
+
+    def save(self, items):
+        with self.db:
+            self.db.executemany(
+                f"INSERT INTO {self.collection}(rowid, embedding) VALUES (?, ?)",
+                [[item[0], serialize_f32(item[1])] for item in items],
+            )
+
+    def search(self, query, k=10):
+        rows = self.db.execute(
+            f"SELECT rowid, distance FROM {self.collection} WHERE embedding MATCH ? AND k = {k} ORDER BY distance",
+            [serialize_f32(query)],
+        ).fetchall()
+        return rows
+
+
+if __name__ == "__main__":
+    EMBEDDINGS_DB_PATH = "./test.db"
+
+    items = [
+        (1, [0.1, 0.1, 0.1, 0.1, 0.1, 0.1]),
+        (2, [0.2, 0.2, 0.2, 0.2, 0.2, 0.2]),
+        (3, [0.3, 0.3, 0.3, 0.3, 0.3, -0.3]),
+        (4, [0.4, 0.4, 0.4, 0.4, 0.4, 0.4]),
+        (5, [0.5, 0.5, 0.5, 0.5, 0.5, -0.5]),
+    ]
+    query = [0.3, 0.3, 0.3, 0.3, 0.3, -0.3]
+
+    vectorDB = VectorDB(EMBEDDINGS_DB_PATH, "test", 6)
+    vectorDB.delete_all()
+    vectorDB.save(items)
+    print(vectorDB.search(query))
