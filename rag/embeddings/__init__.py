7f1f6508090e5046ce0a0676e339bb08b8abb3cb
araobp <araobp@users.noreply.github.com>
2024-10-05 20:02:12 +0900

    update

--- rag/embeddings/__init__.py
+++ rag/embeddings/__init__.py
@@ -12,7 +12,12 @@ DIMENSION = _DIMENSIONS[EMBEDDING_MODEL]
 client = OpenAI()
 
 def get_embedding(text, model=EMBEDDING_MODEL):
-   return client.embeddings.create(input = [text], model=model).data[0].embedding
+    if type(text) == str: 
+        return client.embeddings.create(input = [text], model=model).data[0].embedding
+    elif type(text) == list:
+        result = client.embeddings.create(input = text, model=model)
+        return [e.embedding for e in result.data] 
+    else raise Exception("Type error!")
 
 def ui():
 
